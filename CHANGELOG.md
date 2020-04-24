408069856d018023ce70d4963be1dde13c149473
arakilian0 <arakilian0@gmail.com>
2020-04-24 14:57:48 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## v0.1.2 - Initial Commit
+- Installed [@toolio/log](https://www.npmjs.com/package/@toolio/log)
+- Module exports an object containing a very simple set of configs for @toolio/log
+- Soon, will also have validation schema for @toolio/log 'strict' mode
+- Also added documentation/meta files... (tooliojs.com under construction)
---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = {
+    log: {
+        prefix: 'toolio',
+        console: {
+            on: true,
+            exclude: ['test']
+        },
+        type: {
+            error: {
+                prefix: 'error',
+                color: 'red',
+                bold: true
+            },
+            warning: {
+                prefix: 'warning',
+                color: 'yellow',
+                bold: true
+            },
+            success: {
+                prefix: 'success',
+                color: 'green',
+                bold: true
+            },
+            test: {
+                prefix: 'test',
+                color: 'magenta'
+            }
+        }
+    }
+}
